76f08ecf0dde60c831e3227cf2c6e5f9f8a01397
2272978668@qq.com <2272978668@qq.com>
2019-07-02 09:45:00 +0800

    run code

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -16,18 +16,18 @@ public class DemoApplicationTests {
     @Test
     public void contextLoads() {
     }
-
-    @Test
-    public void status() throws Exception{
-        String[] cmds = {"/bin/sh","-c","ps -ef|grep java"};
-        Process pro = Runtime.getRuntime().exec(cmds);
-        pro.waitFor();
-        InputStream in = pro.getInputStream();
-        BufferedReader read = new BufferedReader(new InputStreamReader(in));
-        String line = null;
-        while((line = read.readLine())!=null){
-            System.out.println(line);
-        }
-    }
+//
+//    @Test
+//    public void status() throws Exception{
+//        String[] cmds = {"/bin/sh","-c","ps -ef|grep java"};
+//        Process pro = Runtime.getRuntime().exec(cmds);
+//        pro.waitFor();
+//        InputStream in = pro.getInputStream();
+//        BufferedReader read = new BufferedReader(new InputStreamReader(in));
+//        String line = null;
+//        while((line = read.readLine())!=null){
+//            System.out.println(line);
+//        }
+//    }
 
 }
